0a72a1db5b67005d124a3f605384872c00d4ef61
Amnah <a.dugheshim@gmail.com>
2024-07-04 00:35:01 +0300

    add partial select node function

--- super-tree-checkbox/src/SuperTreeCheckbox.js
+++ super-tree-checkbox/src/SuperTreeCheckbox.js
@@ -27,28 +27,57 @@ export class SuperTreeCheckbox extends LitElement {
   }
 
   render() {
-    return html`${this.createDom(this.tree)} `;
+    this.partialSelect();
+    // console.log(this.tree)
+    console.log(this.createDom())
+    return html`${this.createDom()} `;
   }
 
-  createDom(tree, left = 0) {
+  createDom(tree = this.tree, left = 0) {
     const dom = document.createElement("div");
-    //TODO add click event to reflect parent check
-    dom.style.position = `relative`
-    dom.style.left = `${left}px`
-    console.log(tree);
+    dom.style.position = `relative`;
+    dom.style.left = `${left}px`;
     for (const treeNode of tree) {
+      const div = document.createElement("div");
       const node = document.createElement("input");
       const label = document.createElement("label");
       node.setAttribute("type", "checkbox");
       node.setAttribute("id", treeNode.key);
+      node.checked = treeNode.selected;
+      console.log(node.checked , treeNode.value)
       label.setAttribute("for", treeNode.key);
       label.append(treeNode.value);
-      dom.append(node);
-      dom.append(label);
+      div.append(node);
+      div.append(label);
+      dom.append(div);
+      node.onclick = this.nodeClicked.bind(this, treeNode);
       if (treeNode.children) {
         dom.append(this.createDom(treeNode.children, 10));
       }
     }
     return dom;
+  }
+  nodeClicked(node) {
+    node.selected = !node.selected
+    if(node.selected && node.children)
+      for (const child of node.children) {
+        child.selected = true
+      }
+    this.render();
+  }
+  partialSelect(nodeTree = this.tree) {
+    for (const treeNode of nodeTree) {
+      this.select(treeNode);
+    }
+  }
+  select(node) {
+    let count = 0;
+    if (node.children)
+      for (let child of node.children) {
+        if (this.select(child)) count++;
+      }
+
+    node.selected = node.selected || count == node?.children?.length
+    return node.selected
   }
 }
